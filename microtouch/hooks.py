13073a2e4e15e2a5c02762a2bd168aa6cc23387b
anwarpatelnoori <anwarpatelrazvi@gmail.com>
2024-06-06 14:01:13 +0530

    none

--- microtouch/hooks.py
+++ microtouch/hooks.py
@@ -257,7 +257,7 @@ override_doctype_class = {
 # 	"frappe.desk.doctype.event.event.get_events": "microtouch.event.get_events"
 # }
 override_whitelisted_methods = {
-	"erpnext.setup.doctype.company.company.is_deletion_job_running": "microtouch.microtouch.company.is_deletion_job_running"
+	"erpnext.setup.doctype.company.company.is_deletion_job_running": "microtouch.company.is_deletion_job_running"
 }
 # /home/st/frappe-virtual-env/mtouch-bench/apps/microtouch.microtouch.company.py
 #
